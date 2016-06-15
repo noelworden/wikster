dac8f5f688415d7f8cc575ed9ec17514132799a8
Noel Worden <noelworden@gmail.com>
2016-06-14 21:24:57 -0600

    closes #2. user can sign in, receive email, and sign out. still need to include username

--- db/schema.rb
+++ db/schema.rb
@@ -27,13 +27,12 @@ ActiveRecord::Schema.define(version: 20160614132749) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
