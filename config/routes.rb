8e1bb51bdf8c6fda0d5bb38e4f209247c62239ac
Noel Worden <noelworden@gmail.com>
2016-06-27 22:01:00 -0600

    user can now upgrade and downgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  resources :users, only: [:show] do
+    post 'downgrade'
+  end
   resources :wikis
 
   resources :charges, only: [:new, :create]
