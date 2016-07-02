ed1abc4806edf4e1bd554c78706208e5f5eea96c
Noel Worden <noelworden@gmail.com>
2016-07-02 10:49:02 -0600

    fixed checkbox form so it properly registers private tag

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,6 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
+
+  scope :visible_to, -> (user) { user ? all : where(private: false)}
+  # scope :visible_to, -> (admin) { admin ? all : where(private: false || true)}
 end
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -3,13 +3,4 @@ class WikiPolicy < ApplicationPolicy
   def index?
     true
   end
-
-  # def update?
-  #   user.admin?
-  # end
-
-  # def edit?
-  #   update?
-  # end
-  
 end
