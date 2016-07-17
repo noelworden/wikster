95734ccad93abd20ba190dc53934989324eaf137
Noel Worden <noelworden@gmail.com>
2016-07-17 16:22:09 -0600

    modified config files for Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ Rails.application.configure do
 
   # For emailing with Heroku and Sendgrid
   config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'wikster.heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
 end
