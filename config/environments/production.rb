8928bd0fcf2fa2962c28513cf0ca51cf94c35128
Noel Worden <noelworden@gmail.com>
2016-07-17 16:51:16 -0600

    syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,6 @@ Rails.application.configure do
   # For emailing with Heroku and Sendgrid
   config.action_mailer.default_url_options = { :host => 'wikster.herokuapp.com'}
   ActionMailer::Base.smtp_settings = {
-    :address => "smpt.sendgrid.net",
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'wikster.heroku.com',
