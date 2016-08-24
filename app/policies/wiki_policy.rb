8f0d791a318ffd8f48a1729bb48ed4aa851ba8fa
Noel Worden <noelworden@gmail.com>
2016-08-23 20:15:37 -0600

    show edit button for collabs. dont display collab options for collabs. dont show private checkbox for collabs

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -3,4 +3,16 @@ class WikiPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def update?
+    user.present? && (record.user == user || user.admin? || record.users.include?(user)) # get collaborators
+  end
+
+  def edit?
+    update?
+  end
+
+  def can_edit_collaborators?
+     user.present? && (record.user == user || user.admin?)
+  end
 end
